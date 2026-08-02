6e2569a2cb6e7da455abd82c02111541d1d25300
braindeadlabz-prog <75919744+Dead-world@users.noreply.github.com>
2026-08-02 14:40:20 -0400

    Added Supabase

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "../supabaseClient";
 
 export default function SignupPage() {
   const [email, setEmail] = useState("");
